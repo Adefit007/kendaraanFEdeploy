e8e838d461b98a1df3e04b8f2982bc18be073a38
Ade Fitriana <adefitriana007@gmail.com>
2022-10-11 04:36:58 +0700

    update

--- src/config/api.jsx
+++ src/config/api.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const API = axios.create(
   {
-    baseURL: "http://localhost:8080/api/v1/",
+    baseURL: "https://kendaraan-apk.herokuapp.com/api/v1/",
   }
   //   {
   //   baseURL: process.env.REACT_APP_BASEURL,
